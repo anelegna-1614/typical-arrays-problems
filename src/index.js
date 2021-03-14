ec7a11192bcff6c1b28560f4897ea861f74bbb6b
anelegna-1614 <anelegna-1614@tut.by>
2021-03-14 21:18:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
+  if (array === [] || array === undefined || array.length === 0) {
   return 0;
+} 
+return Math.min.apply(this, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === [] || array === undefined || array.length === 0) {
+    return 0;
+  } return Math.max.apply(this, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === [] || array === undefined || array.length === 0) {
+    return 0;
+  } return array.reduce((acumulator, currentValue) => (acumulator + currentValue)) / array.length;
 }
